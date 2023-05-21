2b66d2faadf8b86be719e0c9db80ee9ab9f0276b
Moisés Santana V <moysantanav@gmail.com>
2023-05-21 15:43:35 -0600

    [edit] Adding weight fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import { Poppins } from 'next/font/google'
 
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['400', '700']
+  weight: ['200', '400', '500', '700']
 })
 
 export const metadata = {
