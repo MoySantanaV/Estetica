eff858f7edd2065ae0f1fa2d56b51e35b893fe01
Moisés Santana V <moysantanav@gmail.com>
2023-06-24 21:49:10 -0600

    feat/GenericChanges

--- src/app/agenda/page.jsx
+++ src/app/agenda/page.jsx
@@ -2,7 +2,7 @@ import style from '../page.module.css'
 import Appointments from "@/components/appointments/Appointments"
 import Calendar from "@/components/calendar/Calendar"
 
-export default function page() {
+export default function Agenda() {
   return (
     <div className={style.container}>
        <div className={style.centeredContent}>
